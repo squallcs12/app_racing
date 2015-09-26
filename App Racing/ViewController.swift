b378280b14302badcf1142dcd7dce1c3e7d60eb6
Hat Dao <enjoy3013@gmail.com>
2015-09-26 12:59:47 +0700

    Initial Commit

---
+++ App Racing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App Racing
+//
+//  Created by Hat Dao on 9/26/15.
+//  Copyright © 2015 Bang Dao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
